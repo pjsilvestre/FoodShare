16be2eec5c562efd521a7a7277222354faff8524
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-22 06:15:33 -0800

    Implement logout
    Registration:

--- express-app/app.js
+++ express-app/app.js
@@ -1,15 +1,16 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const methodOverride = require("method-override");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var registerRouter = require("./routes/register");
-var loginRouter = require("./routes/login");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const registerRouter = require("./routes/register");
+const loginRouter = require("./routes/login");
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
