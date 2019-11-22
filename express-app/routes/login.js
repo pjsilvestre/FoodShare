abb7a9e41798b581a259e73bb2fb59ca579f00ba
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-22 05:08:08 -0800

    Implement basic login
    No logout functionality

--- express-app/routes/login.js
+++ express-app/routes/login.js
@@ -1,9 +1,33 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const firebase = require('../config/firebase-config-client');
 
 /* GET login page */
 router.get("/", function(req, res, next) {
   res.render("login");
+});
+
+/* POST login page */
+router.post("/", function(req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === "auth/wrong-password") {
+        alert("Wrong password.");
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+    });
+
+  res.redirect("/");
 });
 
 module.exports = router;
