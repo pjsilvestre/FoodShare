dd342ad416c8619fd49b457a1c321ccf0139e8a5
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-22 02:29:26 -0800

    Refactor / enforce code style

--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -1,11 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const admin = require('../config/firebase-config')
-
+const admin = require("../config/firebase-config");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
