711118291f91cc1fbd9940ef96ea98da4e7591a8
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-22 04:36:10 -0800

    Implement basic registration

--- express-app/routes/register.js
+++ express-app/routes/register.js
@@ -2,9 +2,34 @@ const express = require("express");
 const router = express.Router();
 const admin = require("../config/firebase-config");
 
-/* GET login page. */
+/* GET register page. */
 router.get("/", function(req, res, next) {
   res.render("register");
+});
+
+/* POST register page. */
+router.post("/", function(req, res, next) {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  admin
+    .auth()
+    .createUser({
+      uid: username,
+      email: email,
+      emailVerified: false,
+      password: password,
+      disabled: false
+    })
+    .then(function(userRecord) {
+      // See the UserRecord reference doc for the contents of userRecord.
+      console.log("Successfully created new user:", userRecord);
+    })
+    .catch(function(error) {
+      console.log("Error creating new user:", error);
+    });
+  res.redirect("/");
 });
 
 module.exports = router;
