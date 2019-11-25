d50e49b4e0ecb7c4bcdf80a7874ec30cdeeb6ef6
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-25 01:16:43 -0800

    Add user authentication check to donation board page

--- express-app/routes/donation-board.js
+++ express-app/routes/donation-board.js
@@ -4,7 +4,13 @@ const firebase = require("../config/firebase-config-client");
 
 /* GET donation-board page */
 router.get("/", (req, res) => {
-    res.render("donation-board");
-})
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      res.render("donation-board", { user: user });
+    } else {
+      res.render("donation-board");
+    }
+  });
+});
 
 module.exports = router;
