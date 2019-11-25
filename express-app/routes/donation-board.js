dc1a4f3591dc3e0c92f44d6b55a9adafb77131f4
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-25 04:22:44 -0800

    Implement initial donation board

--- express-app/routes/donation-board.js
+++ express-app/routes/donation-board.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const admin = require("../config/firebase-config-admin");
 const firebase = require("../config/firebase-config-client");
+
+const database = admin.firestore();
 
 /* GET donation-board page */
 router.get("/", (req, res) => {
-  firebase.auth().onAuthStateChanged(user => {
+  let donations = firebase.auth().onAuthStateChanged(async user => {
+    await database
+      .collection("donations")
+      .get()
+      .then(snapshot => {
+        donations = snapshot.docs.map(document => {
+          let donation = document.data();
+          donation.id = document.id;
+          return donation;
+        });
+      });
+
     if (user) {
-      res.render("donation-board", { user: user });
+      res.render("donation-board", { user: user, donations });
     } else {
-      res.render("donation-board");
+      res.render("donation-board", { donations });
     }
   });
 });
