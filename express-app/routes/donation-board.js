75fb378896451765e7ac4141b90ffb2682fc709a
pjsilvestre <patrickjohnsilvestre@gmail.com>
2019-11-30 09:07:22 -0800

    Add async-await
    Ensures donation-board pulls latest data from Firestore...hopefully

--- express-app/routes/donation-board.js
+++ express-app/routes/donation-board.js
@@ -1,27 +1,27 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const admin = require("../config/firebase-config-admin");
-const firebase = require("../config/firebase-config-client");
+const admin = require('../config/firebase-config-admin');
+const firebase = require('../config/firebase-config-client');
 
 const database = admin.firestore();
 
 /* GET donation-board page */
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   let donations = firebase.auth().onAuthStateChanged(async user => {
     await database
-      .collection("donations")
+      .collection('donations')
       .get()
       .then(snapshot => {
         donations = snapshot.docs.map(document => {
           let donation = document.data();
           donation.id = document.id;
 
-          let dietary_restrictions = "";
-          if (donation.halal) dietary_restrictions += "Halal ";
-          if (donation.kosher) dietary_restrictions += "Kosher ";
-          if (donation.pescatarian) dietary_restrictions += "Pescatarian ";
-          if (donation.vegan) dietary_restrictions += "Vegan ";
-          if (donation.vegetarian) dietary_restrictions += "Vegetarian";
+          let dietary_restrictions = '';
+          if (donation.halal) dietary_restrictions += 'Halal ';
+          if (donation.kosher) dietary_restrictions += 'Kosher ';
+          if (donation.pescatarian) dietary_restrictions += 'Pescatarian ';
+          if (donation.vegan) dietary_restrictions += 'Vegan ';
+          if (donation.vegetarian) dietary_restrictions += 'Vegetarian';
 
           donation.dietary_restrictions = dietary_restrictions;
 
@@ -29,11 +29,10 @@ router.get("/", (req, res) => {
         });
       });
 
-    console.log(donations);
     if (user) {
-      res.render("donation-board", { user: user, donations });
+      res.render('donation-board', { user: user, donations });
     } else {
-      res.render("donation-board", { donations });
+      res.render('donation-board', { donations });
     }
   });
 });
