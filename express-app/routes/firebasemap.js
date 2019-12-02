d1e7464a43bd69a5762605148b433c1375e2156d
JesWu <jeffreywu1997@gmail.com>
2019-12-02 00:25:31 -0800

    Added map to donation-map

--- express-app/routes/firebasemap.js
+++ express-app/routes/firebasemap.js
@@ -10,21 +10,10 @@ router.get("/", (req, res) => {
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
       res.render("donation-map", { user: user });
-      initMap();
     } else {
       res.redirect("/");
     }
   });
 });
-
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
 
   module.exports = router;
