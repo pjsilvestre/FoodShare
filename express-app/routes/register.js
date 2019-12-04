5f581415e52a35d0decba8b5da7b0773308aa4e9
ryanhhong <ryanhhong@outlook.com>
2019-12-04 01:07:50 -0800

    added messaging system2

--- express-app/routes/register.js
+++ express-app/routes/register.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const admin = require('../config/firebase-config-admin');
 
+
+
 /* GET register page. */
 router.get('/', (req, res) => {
   res.render('register');
@@ -12,7 +14,7 @@ router.post('/', (req, res) => {
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
-
+ 
   admin
     .auth()
     .createUser({
